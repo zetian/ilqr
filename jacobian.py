2d25006891da067f69ceee17008a40460113be7a
zetian <zetianzzt@gmail.com>
2019-06-29 01:20:44 -0700

    add state constraint vis

--- jacobian.py
+++ jacobian.py
@@ -7,6 +7,7 @@ import scipy.sparse as sparse
 from scipy.linalg import block_diag
 from systems import *
 from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
 
 
 class iterative_MPC_optimizer:
@@ -54,17 +55,28 @@ class iterative_MPC_optimizer:
     
     def plot(self):
         plt.figure(figsize=(8*1.1, 6*1.1))
+        currentAxis = plt.gca()
         plt.title('MPC: 2D, x and y.  ')
         plt.axis('equal')
         plt.plot(noisy_targets[:, 0], noisy_targets[:, 1], '--r', label='Target', linewidth=2)
-        plt.plot(self.states[:, 0], self.states[:, 1], '-+b', label='MPC', linewidth=1.0)
+        plt.plot(self.states[:, 0], self.states[:, 1], '-+k', label='MPC', linewidth=1.0)
         plt.xlabel('x (meters)')
         plt.ylabel('y (meters)')
+        for i in range(self.horizon):
+            x = self.xmin[i, 0]
+            size_x = abs(self.xmax[i, 0] - self.xmin[i, 0])
+            y = self.xmin[i, 1]
+            size_y = abs(self.xmax[i, 1] - self.xmin[i, 1])
+            currentAxis.add_patch(Rectangle((x, y), size_x, size_y, alpha=1))
         # plt.figure(figsize=(8*1.1, 6*1.1))
         # plt.title('iLQR: state vs. time.  ')
         # plt.plot(self.states[:, 2], '-b', linewidth=1.0, label='speed')
         # plt.plot(ref_vel, '-r', linewidth=1.0, label='target speed')
         # plt.ylabel('speed')
+        # plt.figure(figsize=(8*1.1, 6*1.1))
+        # plt.title('iLQR: input vs. time.  ')
+        # plt.plot(self.inputs[:, 1], '-b', linewidth=1.0, label='turning rate')
+        # plt.ylabel('inputs')
         plt.show()
     
     def __call__(self):
@@ -89,12 +101,6 @@ class iterative_MPC_optimizer:
         xmax = self.xmax.ravel()
         lineq = np.hstack([xmin, np.kron(np.ones(self.horizon - 1), umin)])
         uineq = np.hstack([xmax, np.kron(np.ones(self.horizon - 1), umax)])
-        # xmin = np.array([0, 0, 0.8, -np.inf])
-        # xmax = np.array([20, 20, 1.2, np.inf])
-        # xmin = np.array([-np.inf, -np.inf, 0, -np.inf])
-        # xmax = np.array([np.inf, np.inf, 2, np.inf])
-        # lineq = np.hstack([np.kron(np.ones(self.horizon), xmin), np.kron(np.ones(self.horizon - 1), umin)])
-        # uineq = np.hstack([np.kron(np.ones(self.horizon), xmax), np.kron(np.ones(self.horizon - 1), umax)])
 
         self.states = self.target_states
         self.min_cost = np.inf
@@ -176,8 +182,8 @@ v_max = 11
 system = Car()
 system.set_dt(dt)
 system.set_cost(np.diag([50.0, 50.0, 10.0, 1.0]), np.diag([300.0, 1000.0]))
-system.Q_f = system.Q*horizon#*50
-system.set_control_limit(np.array([[-1.5, 1.5], [-0.2, 0.2]]))
+system.Q_f = system.Q*horizon/100#*50
+system.set_control_limit(np.array([[-1, 1], [-0.2, 0.2]]))
 init_inputs = np.zeros((ntimesteps - 1, num_input))
 
 
@@ -191,8 +197,8 @@ for i in range(1, ntimesteps):
     target_states[i, 1] = target_states[i-1, 1] + np.sin(target_states[i-1, 3])*dt*ref_vel[i - 1]
     target_states[i, 2] = ref_vel[i]
     target_states[i, 3] = target_states[i-1, 3] + curv*dt
-    noisy_targets[i, 0] = target_states[i, 0] + random.uniform(0, 1)
-    noisy_targets[i, 1] = target_states[i, 1] + random.uniform(0, 1)
+    noisy_targets[i, 0] = target_states[i, 0] + random.uniform(0, 0.5)
+    noisy_targets[i, 1] = target_states[i, 1] + random.uniform(0, 0.5)
     noisy_targets[i, 2] = ref_vel[i]
     noisy_targets[i, 3] = target_states[i, 3] + random.uniform(0, 0.1)
 
@@ -224,14 +230,14 @@ for i in range(1, ntimesteps):
 
 # State constraints
 for i in range(ntimesteps):
-    xmax[i, 0] = noisy_targets[i, 0] + 2
-    xmax[i, 1] = noisy_targets[i, 1] + 2
-    xmax[i, 2] = noisy_targets[i, 2] + 1
-    xmax[i, 3] = noisy_targets[i, 3] + 1
-    xmin[i, 0] = noisy_targets[i, 0] - 2
-    xmin[i, 1] = noisy_targets[i, 1] - 2
-    xmin[i, 2] = noisy_targets[i, 2] - 1
-    xmin[i, 3] = noisy_targets[i, 3] - 1
+    xmax[i, 0] = target_states[i, 0] + 0.5
+    xmax[i, 1] = target_states[i, 1] + 0.5
+    xmax[i, 2] = target_states[i, 2] + 0.1
+    xmax[i, 3] = target_states[i, 3] + 0.1
+    xmin[i, 0] = target_states[i, 0] - 0.5
+    xmin[i, 1] = target_states[i, 1] - 0.5
+    xmin[i, 2] = target_states[i, 2] - 0.1
+    xmin[i, 3] = target_states[i, 3] - 0.1
 
 
 start = time.time()
